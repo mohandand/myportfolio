69743f08ea217f1c3116001edb807d9e8f74d023
mohandand <mohandand@gmail.com>
2022-01-13 17:09:47 -0800

    updated code

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,39 @@
+import React,{useState ,useEffect} from 'react';
+
 
 const Header = () => {
+    const [toggleMenu, setToggleMenu] = useState(false);
+    const [screenWidth, setScreenWidth] = useState(window.innerWidth)
+
+    const toggleNav = () => {
+        setToggleMenu(!toggleMenu)
+    }
+
+    useEffect(() => {
+
+        const changeWidth = () => {
+          setScreenWidth(window.innerWidth);
+        }
+    
+        window.addEventListener('resize', changeWidth)
+
+        return () => {
+            window.removeEventListener('resize', changeWidth)
+        }
+    
+      }, [])
+
     return(
         <div className="headercontainer">
             <div className="myname">Mohan Dandigam</div>
-            <ul className="menu">
-                <li>Home</li>
-                <li>Resume</li>
-                <li>Works</li>
-                <li>About</li>
-                <li>Contact</li>
-            </ul>
+            {(toggleMenu || screenWidth > 500) && <ul className="menu">
+                <li className="menuitem">Home</li>
+                <li className="menuitem">Resume</li>
+                <li className="menuitem">Works</li>
+                <li className="menuitem">About</li>
+                <li className="menuitem">Contact</li>
+            </ul>}
+            <img className="mobile-menu" onClick={toggleNav}src="images/menu.JPG" alt="Open Navigation"/>
         </div>
     )
 }
